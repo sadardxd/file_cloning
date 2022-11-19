d5366191d24a7f5394e26ed3d6c186068fe741f6
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-11-19 17:10:10 +0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,25 @@
+import os
+
+ 
+
+if __name__ == "__main__":
+
+ 
+
+   try:
+
+ 
+
+       os.system("git pull")
+
+ 
+
+       __import__("Filexyz").Younis_johnxyz()
+
+ 
+
+   except Exception as e:
+
+ 
+
+       exit(str(e))
